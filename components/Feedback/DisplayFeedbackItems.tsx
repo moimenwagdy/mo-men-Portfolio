10de9f881ceada760e198e1dd76feae98e9e0c0e
moimenwagdy <moimenwy@gmail.com>
2024-08-11 07:19:50 +0300

    api Test

--- components/Feedback/DisplayFeedbackItems.tsx
+++ components/Feedback/DisplayFeedbackItems.tsx
@@ -1,25 +1,25 @@
-import FeedbackItem from "./FeedbackItem";
+// import FeedbackItem from "./FeedbackItem";
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+// console.log(apiUrl);
+// const DisplayFeedbackItems = async () => {
+//   const response = await fetch(`${apiUrl}/feedback/`);
+//   const feedbackArray = (await response.json()) as {
+//     name: string;
+//     message: string;
+//     date: Date;
+//     _id: string;
+//   }[];
+//   return (
+//     <section
+//       id="feedbackViewer"
+//       className="w-full h-96 overflow-y-auto sm:w-3/4 md:w-1/2">
+//       <ul>
+//         {feedbackArray.map((feedback) => {
+//           return <FeedbackItem key={feedback._id} {...feedback} />;
+//         })}
+//       </ul>
+//     </section>
+//   );
+// };
 
-const DisplayFeedbackItems = async () => {
-  const response = await fetch(`${process.env.API_URL}/feedback/`);
-  const feedbackArray = (await response.json()) as {
-    name: string;
-    message: string;
-    date: Date;
-    _id: string;
-  }[];
-  console.log(feedbackArray);
-  return (
-    <section
-      id="feedbackViewer"
-      className="w-full h-96 overflow-y-auto sm:w-3/4 md:w-1/2">
-      <ul>
-        {feedbackArray.map((feedback) => {
-          return <FeedbackItem key={feedback._id} {...feedback} />;
-        })}
-      </ul>
-    </section>
-  );
-};
-
-export default DisplayFeedbackItems;
+// export default DisplayFeedbackItems;
--- components/Feedback/function/sendFeedback.ts
+++ components/Feedback/function/sendFeedback.ts
@@ -1,45 +1,47 @@
-"use server";
-import { revalidatePath } from "next/cache";
+// "use server";
+// import { revalidatePath } from "next/cache";
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+ 
+// console.log(apiUrl);
+// export const sendFeedBack = async (_prvState: any, formData: any) => {
+//   const data = {
+//     name: formData.get("userName"),
+//     message: formData.get("userFeedback"),
+//     date: new Date().toISOString(),
+//   };
+//   if (
+//     !data.name ||
+//     !data.message ||
+//     data.name.trim() === "" ||
+//     data.message.trim() === ""
+//   ) {
+//     return {
+//       success: false,
+//       message: "Name and feedback message cannot be empty.",
+//     };
+//   }
 
-export const sendFeedBack = async (_prvState: any, formData: any) => {
-  const data = {
-    name: formData.get("userName"),
-    message: formData.get("userFeedback"),
-    date: new Date().toISOString(),
-  };
-  if (
-    !data.name ||
-    !data.message ||
-    data.name.trim() === "" ||
-    data.message.trim() === ""
-  ) {
-    return {
-      success: false,
-      message: "Name and feedback message cannot be empty.",
-    };
-  }
-
-  try {
-    const response = await fetch(`${process.env.API_URL}/feedback/`, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!response.ok) {
-      return {
-        success: false,
-        message: "sending failed!, please try again later.",
-      };
-    }
-    revalidatePath("/");
-    const responsed = await response.json();
-    return {
-      success: true,
-      data: responsed,
-    };
-  } catch (error) {
-    throw new Error("failed to send feedback");
-  }
-};
+//   try {
+//     const response = await fetch(`${apiUrl}/feedback/`, {
+//       method: "POST",
+//       body: JSON.stringify(data),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     if (!response.ok) {
+//       return {
+//         success: false,
+//         message: "sending failed!, please try again later.",
+//       };
+//     }
+//     revalidatePath("/");
+//     const responsed = await response.json();
+//     return {
+//       success: true,
+//       data: responsed,
+//     };
+//   } catch (error) {
+//     throw new Error("failed to send feedback");
+//   }
+// };
